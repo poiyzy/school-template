4c8a2e26a427a4d4323cbb6ba1109b6a4a809775
Roy Young <zhuoyuyang@gmail.com>
2011-12-01 13:28:14 +0800

    修改single图片链接

--- single.php
+++ single.php
@@ -16,7 +16,7 @@ li{
 }
 img{border:none;}
 body {
-	background-image:url(images/bg_tile.jpg);
+	background-image:url(<?php bloginfo('template_url'); ?>/images/bg_tile.jpg);
 	background-repeat: repeat;
 }
 </style>
